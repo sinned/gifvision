87b083bf101cf45f50d16b860de7e7ae732ab96b
Dennis Yang <sinned@gmail.com>
2012-10-30 11:43:11 -0700

    added GA

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>Yeehaw.</title>
+    <title>GifVision.</title>
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
   <script src="js/jquery.backstretch.min.js"></script>
   <script type="text/javascript">
@@ -17,7 +17,6 @@
 			"http://i.minus.com/iblNcxynRn8sdR.gif",
 			"http://i.imgur.com/UKlN7.gif",
 			"http://i.imgur.com/4xGNI.gif",
-			"http://www.yapfiles.ru/files/478176/20d7d2ba21a9f90ea7121a7616362668.gif",
 			"http://i.imgur.com/XaYeD.gif",
 			"http://24.media.tumblr.com/tumblr_m33adqW0RX1qacmz1o1_500.gif",
 			"http://i.imgur.com/788bA.gif",
@@ -81,6 +80,19 @@
 stretchImage();
 
 });
+</script>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-35961275-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
 </script>
   </head>
 
